da58ff8d301e7cc5874bfe87122b6593b87ee78d
nutancollege <rawofficial078@gmail.com>
2024-12-16 00:59:23 +0530

    Update pr11.cpp

--- pr11.cpp
+++ pr11.cpp
@@ -1,80 +1,69 @@
-
 #include <iostream>
-#include <stack>
+#include <queue>
 #include <string>
 using namespace std;
 
-bool isOperand(char ch) {
-    return isalnum(ch);
+void addJob(queue<string> &jobQueue, const string &job) {
+    jobQueue.push(job);
+    cout << "Job added: " << job << endl;
 }
 
-int precedence(char op) {
-    return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/') ? 2 : 0;
+void deleteJob(queue<string> &jobQueue) {
+    if (!jobQueue.empty()) {
+        cout << "Job deleted: " << jobQueue.front() << endl;
+        jobQueue.pop();
+    } else {
+        cout << "Queue is empty, no job to delete!" << endl;
+    }
 }
 
-string infixToPostfix(string infix) {
-    stack<char> s;
-    string postfix;
-
-    for (char ch : infix) {
-        if (isOperand(ch)) {
-            postfix += ch;
-            postfix += ' ';
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!s.empty() && precedence(s.top()) >= precedence(ch)) {
-                postfix += s.top();
-                postfix += ' ';
-                s.pop();
-            }
-            s.push(ch);
-        } else {
-            cout << "Invalid character or parentheses not allowed: " << ch << "\n";
-            return "";
+void displayJobs(const queue<string> &jobQueue) {
+    if (jobQueue.empty()) {
+        cout << "Job queue is empty." << endl;
+    } else {
+        queue<string> tempQueue = jobQueue;
+        cout << "Jobs in the queue:" << endl;
+        while (!tempQueue.empty()) {
+            cout << tempQueue.front() << endl;
+            tempQueue.pop();
         }
     }
-
-    while (!s.empty()) {
-        postfix += s.top();
-        postfix += ' ';
-        s.pop();
-    }
-
-    return postfix;
-}
-
-int evaluatePostfix(string postfix) {
-    stack<int> s;
-
-    for (char ch : postfix) {
-        if (ch == ' ') continue;
-        if (isdigit(ch)) {
-            s.push(ch - '0');
-        } else {
-            int b = s.top(); s.pop();
-            int a = s.top(); s.pop();
-            s.push(ch == '+' ? a + b : ch == '-' ? a - b : ch == '*' ? a * b : a / b);
-        }
-    }
-
-    return s.top();
 }
 
 int main() {
-    string infix;
-    cout << "Enter an infix expression (single-character operands and operators): ";
-    cin >> infix;
+    queue<string> jobQueue;
+    int choice;
+    string job;
 
-    string postfix = infixToPostfix(infix);
-    if (postfix.empty()) return 0;
+    do {
+        cout << "\n--- Job Queue Menu ---" << endl;
+        cout << "1. Add Job" << endl;
+        cout << "2. Delete Job" << endl;
+        cout << "3. Display Jobs" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    cout << "Postfix expression: " << postfix << endl;
-
-    if (postfix.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") != string::npos) {
-        cout << "Alphabetic characters found, evaluation skipped.\n";
-    } else {
-        cout << "Result of evaluation: " << evaluatePostfix(postfix) << endl;
-    }
+        switch (choice) {
+            case 1:
+                cout << "Enter the job name: ";
+                cin >> job;
+                addJob(jobQueue, job);
+                break;
+            case 2:
+                deleteJob(jobQueue);
+                break;
+            case 3:
+                displayJobs(jobQueue);
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+                break;
+        }
+    } while (choice != 4);
 
     return 0;
 }
-
