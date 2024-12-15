7ebd6634d1ef9c8673eb8e5ddc68db5f5a674bc8
nutancollege <rawofficial078@gmail.com>
2024-12-16 00:56:17 +0530

    Update 4pr.py

--- 4pr.py
+++ 4pr.py
@@ -1,55 +1,41 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
 
-def LinearSearch(l, key):
-    for i in range(len(l)):
-        if l[i] == key:
-            return i
-    return -1
+def display_top_scores(arr, top_n=5):
+    print(f"Top {top_n} scores:")
+    for score in arr[-top_n:]:
+        print(score)
 
-def SentinelSearch(l, key):
-    l.append(key)
-    i = 0
-    while l[i] != key:
-        i += 1
-    return -1 if i == len(l) - 1 else i
+percentages = []
+num_students = int(input("Enter the number of students: "))
 
-def BinarySearch(l, key):
-    left, right = 0, len(l) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if key == l[mid]:
-            return mid
-        elif key < l[mid]:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return -1
+for i in range(num_students):
+    score = float(input(f"Enter the percentage for student {i + 1}: "))
+    percentages.append(score)
 
-def FibonacciSearch(l, key):
-    fib2, fib1, fib = 0, 1, 1
-    while fib < len(l):
-        fib2, fib1 = fib1, fib
-        fib = fib2 + fib1
-    offset = -1
-    while fib > 1:
-        i = min(offset + fib2, len(l) - 1)
-        if l[i] < key:
-            fib, fib1, fib2 = fib1, fib2, fib - fib1
-            offset = i
-        elif l[i] > key:
-            fib, fib1, fib2 = fib2, fib1 - fib2, fib - fib1
-        else:
-            return i
-    if fib1 and offset + 1 < len(l) and l[offset + 1] == key:
-        return offset + 1
-    return -1
+selection_sort(percentages)
+print("\nSorted percentages using Selection Sort:")
+print(percentages)
+display_top_scores(percentages)
 
-digits = [int(input(f"Enter number {i}: ")) for i in range(int(input("Enter the length of list: ")))]
-key = int(input("Key element to find: "))
-
-print("Linear Search index:", LinearSearch(digits, key))
-print("Sentinel Search index:", SentinelSearch(digits, key))
-print("Binary Search index:", BinarySearch(sorted(digits), key))
-print("Fibonacci Search index:", FibonacciSearch(sorted(digits), key))
-
-
+bubble_sort(percentages)
+print("\nSorted percentages using Bubble Sort:")
+print(percentages)
+display_top_scores(percentages)
