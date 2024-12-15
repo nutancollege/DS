1f8cab25f50f807ea0d9b801d877f73dde2a1ee5
nutancollege <rawofficial078@gmail.com>
2024-12-16 00:59:03 +0530

    Update pr10.cpp

--- pr10.cpp
+++ pr10.cpp
@@ -1,55 +1,40 @@
-
 #include <iostream>
 #include <stack>
-#include <string>
+#include <unordered_map>
 using namespace std;
-// Function to check if the given expression is well parenthesized
-bool isWellParenthesized(string expression) {
-stack<char> s;
-for (char ch : expression) {
-// Push opening brackets onto the stack
-if (ch == '(' || ch == '{' || ch == '[') {
-s.push(ch);
-}
-// Check for closing brackets
-else if (ch == ')' || ch == '}' || ch == ']') {
-// If stack is empty or top of stack doesn't match
-if (s.empty()) return false;
-char top = s.top();
-s.pop();
-// Check if the brackets match
-if ((ch == ')' && top != '(') ||
-(ch == '}' && top != '{') ||
-(ch == ']' && top != '[')) {
-return false;
-}
-}
-}
-// If stack is empty, all opening brackets matched
-return s.empty();
-}
-bool hasParentheses(string expression) {
-// Check if the expression contains any parentheses
-for (char ch : expression) {
-if (ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == '[' || ch == ']') {
-return true;
-}
-}
-return false;
-}
-int main() {
-string expression;
-cout << "Enter an expression: ";
-getline(cin, expression);
-if (hasParentheses(expression)) {
-if (isWellParenthesized(expression)) {
-cout << "The expression is well parenthesized.\n";
-} else {
-cout << "The expression is not well parenthesized.\n";
-}
-} else {
-cout << "The expression does not contain any parentheses.\n";
-}
-return 0;
+
+bool isWellParenthesized(const string& expression) {
+    stack<char> s;
+    unordered_map<char, char> brackets = {
+        {')', '('},
+        {'}', '{'},
+        {']', '['}
+    };
+
+    for (char ch : expression) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch);
+        } else if (ch == ')' || ch == '}' || ch == ']') {
+            if (s.empty() || s.top() != brackets[ch]) {
+                return false;
+            }
+            s.pop();
+        }
+    }
+
+    return s.empty();
 }
 
+int main() {
+    string expression;
+    cout << "Enter an expression: ";
+    getline(cin, expression);
+
+    if (isWellParenthesized(expression)) {
+        cout << "The expression is well-parenthesized." << endl;
+    } else {
+        cout << "The expression is not well-parenthesized." << endl;
+    }
+
+    return 0;
+}
