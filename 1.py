1de3d94eb6e3271f5545fb901cf201fcf98f5186
nutancollege <rawofficial078@gmail.com>
2024-12-16 00:54:30 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,81 +1,73 @@
-// 01.
-// Write a Python program to store marks scored in the subject “Fundamentals of Data Structure” by N students in the class. Write functions to compute the following:
-// a) The average score of the class
-// b) The highest score and the lowest score of the class
-// c) The count of students who were absent for the test
-// d) Display the mark with the highest frequency
 
-// Code
-
-def get_student_marks():
-    N = int(input("Enter the number of students: "))
-    marks = []
-    for i in range(N):
-        mark = int(input(f"Enter marks for Student {i + 1} (use -1 if absent): "))
-        marks.append(mark)
-    return marks
-
-def compute_average(marks):
+def Average(marks, n):
     total = 0
-    count = 0
-    for mark in marks:
-        if mark != -1:
-            total += mark
-            count += 1
-    if count == 0:
-        return 0
-    return total / count
+    for i in range(n):
+        total += marks[i]
+    average = total / n
+    return average
 
-def find_highest_lowest(marks):
-    highest = -float('inf')
-    lowest = float('inf')
-    for mark in marks:
-        if mark != -1:
-            if mark > highest:
-                highest = mark
-            if mark < lowest:
-                lowest = mark
-    return highest, lowest
+def Maximum(marks, n):
+    max_marks = marks[0]
+    for i in range(1, n):
+        if marks[i] > max_marks:
+            max_marks = marks[i]
+    return max_marks
 
-def count_absent_students(marks):
-    count = 0
-    for mark in marks:
-        if mark == -1:
-            count += 1
-    return count
+def Minimum(marks, n):
+    min_marks = 999
+    for i in range(n):
+        if marks[i] < min_marks and marks[i] >= 0:
+            min_marks = marks[i]
+    return min_marks
 
-def mark_with_highest_frequency(marks):
-    max_frequency = 0
-    most_frequent_mark = None
-    for i in range(len(marks)):
+def Absent(marks, n):
+    cnt = 0
+    for i in range(n):
         if marks[i] == -1:
-            continue
+            cnt += 1
+    return cnt
 
-        frequency = 0
-        for j in range(len(marks)):
-            if marks[i] == marks[j]:
-                frequency += 1
+def Cntmax(marks, n, s):
+    cntmax = 0
+    for i in range(n):
+        if marks[i] == s:
+            cntmax += 1
+    return cntmax
 
-        if frequency > max_frequency:
-            max_frequency = frequency
-            most_frequent_mark = marks[i]
+marks = []
+n = int(input("Enter number of students, if absent please enter -1: "))
+for i in range(n):
+    m = int(input("Enter your marks: "))
+    marks.append(m)
 
-    return most_frequent_mark
+while True:
+    print("Please press '1' for average")
+    print("'2' for maximum marks")
+    print("'3' for minimum marks")
+    print("'4' for absent students")
+    print("'5' for number of students with maximum marks")
+    print("'6' for none")
 
-def main():
-    marks = get_student_marks()
+    opt = int(input("Enter your choice: "))
 
-    avg_score = compute_average(marks)
-    highest_score, lowest_score = find_highest_lowest(marks)
-    absent_count = count_absent_students(marks)
-    most_frequent_mark = mark_with_highest_frequency(marks)
-
-    print("\nClass Statistics:")
-    print(f"Average Score: {avg_score:.2f}")
-    print(f"Highest Score: {highest_score}")
-    print(f"Lowest Score: {lowest_score}")
-    print(f"Number of Absent Students: {absent_count}")
-    print(f"Mark with Highest Frequency: {most_frequent_mark}")
-
-if __name__ == "__main__":
-    main()
+    if opt == 1:
+        avg = Average(marks, n)
+        print("Average is:", avg)
+    elif opt == 2:
+        max_marks = Maximum(marks, n)
+        print("Maximum marks:", max_marks)
+    elif opt == 3:
+        min_marks = Minimum(marks, n)
+        print("Minimum marks:", min_marks)
+    elif opt == 4:
+        cnt = Absent(marks, n)
+        print("No. of absent students:", cnt)
+    elif opt == 5:
+        max_marks = Maximum(marks, n)
+        cntmax = Cntmax(marks, n, max_marks)
+        print("The number of students having maximum marks:", cntmax)
+    elif opt == 6:
+        print("This option indicates you have chosen none!")
+        break
+    else:
+        print("Invalid option, please try again.")
