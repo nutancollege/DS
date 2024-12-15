2db126323db1163d4e3eadcd5dc03e193cf80fe2
nutancollege <rawofficial078@gmail.com>
2024-12-16 00:55:46 +0530

    Update 3pr.py

--- 3pr.py
+++ 3pr.py
@@ -1,111 +1,120 @@
-/ Code
+def Addition(r, c, m, m1):
+    print("Addition of two matrices:")
+    result = []
+    for i in range(r):
+        row = []
+        for j in range(c):
+            row.append(m[i][j] + m1[i][j])
+        result.append(row)
 
+    print("Using nested loops:")
+    for row in result:
+        print(row)
 
+def Subtraction(r, c, m, m1):
+    print("Subtraction of two matrices:")
+    result = []
+    for i in range(r):
+        row = []
+        for j in range(c):
+            row.append(m[i][j] - m1[i][j])
+        result.append(row)
 
+    print("Using nested loops:")
+    for row in result:
+        print(row)
 
-def addition(matrix1, matrix2, mat1row, mat2col, result):
-    for i in range(0, mat1row):
-        for j in range(0, mat2col):
-            result[i][j] = matrix1[i][j] + matrix2[i][j]
+def Transpose(r, c, m):
+    print("Transpose of the first matrix:")
+    result = []
+    for j in range(c):
+        row = []
+        for i in range(r):
+            row.append(m[i][j])
+        result.append(row)
 
-    print("Addition of two matrix is ")
-    for i in range(0, mat1row):
-        for j in range(0, mat2col):
-            print(result[i][j], end="")
-        print()
+    print("Using nested loops:")
+    for row in result:
+        print(row)
 
-def subtraction(matrix1, matrix2, mat1row, mat2col, result):
-    for i in range(0, mat1row):
-        for j in range(0, mat2col):
-            result[i][j] = matrix1[i][j] - matrix2[i][j]
+def Transpose1(r, c, m1):
+    print("Transpose of the second matrix:")
+    result = []
+    for j in range(c):
+        row = []
+        for i in range(r):
+            row.append(m1[i][j])
+        result.append(row)
 
-    print("Subtraction of two matrix is ")
-    for i in range(0, mat1row):
-        for j in range(0, mat2col):
-            print(result[i][j], end="")
-        print()
+    print("Using nested loops:")
+    for row in result:
+        print(row)
 
-def multiplication(matrix1, matrix2):
-    temp = [[0, 0], [0, 0]]
-    for i in range(len(matrix1)):
-        for j in range(len(matrix2[0])):
-            for k in range(len(matrix2)):
-                temp[i][j] += matrix1[i][k] * matrix2[k][j]
-    print("Multiplication of two matrix is ")
-    for result in temp:
-        print(result)
-def transpose(matrix1, mat1row, mat1col):
-    result = [[0 for x in range(mat1row)] for y in range(mat1col)]
-    
-    for i in range(0, mat1row):
-        for j in range(0, mat1col):
-            result[j][i] = matrix1[i][j]
-    
-    print("Transpose of Matrix 1 is ")
-    for i in range(0, mat1col):
-        for j in range(0, mat1row):
-            print(result[i][j], end=" ")
-        print()
+def Multiplication(r, c, m, m1):
+    print("Multiplication of matrices:")
 
-if __name__ == "__main__":
-    print("INPUT FOR MATRIX 1")
-    mat1row = int(input("Enter the number of rows = "))
-    mat1col = int(input("Enter the number of columns = "))
-    matrix1 = []
-    for i in range(0, mat1row):
-        a = []
-        for j in range(0, mat1col):
-            a.append(int(input()))
-        matrix1.append(a)
+    if r == c:
+        result = []
+        for i in range(r):
+            row = []
+            for j in range(c):
+                sum = 0
+                for k in range(c):
+                    sum += m[i][k] * m1[k][j]
+                row.append(sum)
+            result.append(row)
 
-    print("Matrix 1")
-    for i in range(0, mat1row):
-        for j in range(0, mat1col):
-            print(matrix1[i][j], end=" ")
-        print()
-    
-    print("INPUT FOR MATRIX 2")
-    mat2row=int(input("Enter the number of rows ="))
-    mat2col=int(input("Enter the number of columns ="))
-    
-    matrix2=[]
-    for i in range(0,mat2row):
-        a=[]
-        for j in range(0,mat2col):
-            a.append(int(input()))
-        matrix2.append(a)    
-    
-print("Matrix 2")
-for i in range(0, mat2row):
-    for j in range(0, mat2col):
-        print(matrix2[i][j], end="")
-    print()
+        print("Using nested loops:")
+        for row in result:
+            print(row)
+    else:
+        print("Operation invalid: Matrices must be square i.e (n x n) for multiplication.")
+m = []
+r = int(input("Enter the number of rows: "))
+c = int(input("Enter the number of columns: "))
+for i in range(r):
+    a = []
+    for j in range(c):
+        ele = int(input("Enter element: "))
+        a.append(ele)
+    m.append(a)
 
-result = [[0, 0], [0, 0]]
+print("1st matrix is:")
+for row in m:
+    print(row)
+m1 = []
+for i in range(r):
+    a = []
+    for j in range(c):
+        ele = int(input("Enter element: "))
+        a.append(ele)
+    m1.append(a)
 
-print("")
+print("2nd matrix is:")
+for row in m1:
+    print(row)
+while True:
+    print("Please press 1 for matrix addition;")
+    print("Please press 2 for matrix subtraction;")
+    print("Please press 3 for transpose of matrix 1;")
+    print("Please press 4 for transpose of matrix 2;")
+    print("Please press 5 for multiplication of matrices;")
+    print("Please press 6 for none;")
 
-print("-----SELECT AN OPTION------")
-print("Option 1 : Press 1 to get Addition of Matrix ")
-print("Option 2 : Press 2 to get Subtraction of Matrix ")
-print("Option 3 : Press 3 to get Multiplication of Matrix ")
-print("Option 4 : Press 4 to get Transpose of Matrix ")
-print("Option 5 : Press 0 for Exit")
+    opt = int(input("Enter your choice: "))
 
-while(True):
-    cases = int(input("Enter Your Option Number Here --> "))
-
-    match cases:
-        case 0:
-            print("Exit")
-            break
-        case 1:
-            addition(matrix1, matrix2, mat1row, mat2col, result)
-        case 2:
-            subtraction(matrix1, matrix2, mat1row, mat2col, result)
-        case 3:
-            multiplication(matrix1, matrix2)
-        case 4:
-            transpose(matrix1, mat1row, mat1col)
-        case _:
-            print("OOPS!!! INVALID INPUT!!") 
+    if opt == 1:
+        Addition(r, c, m, m1)
+    elif opt == 2:
+        Subtraction(r, c, m, m1)
+    elif opt == 3:
+        Transpose(r, c, m)
+    elif opt == 4:
+        Transpose1(r, c, m1)
+    elif opt == 5:
+        Multiplication(r, c, m, m1)
+    elif opt == 6:
+        print("You opted for none!")
+        break
+    else:
+        print("Invalid option, please try again.")
